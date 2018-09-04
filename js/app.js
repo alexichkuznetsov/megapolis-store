c4268c92bbf4e3f1dfee0b575af2abc55492a95a
alexichkuznetsov <alexichkuznetsov@gmail.com>
2018-09-04 21:44:34 +0300

    Fixed some sections

--- js/app.js
+++ js/app.js
@@ -3,9 +3,36 @@ var toggleButton = document.querySelector('.main-header__toggle-button');
 var navContainer = document.querySelector('.main-header__navigation');
 
 toggleButton.addEventListener('click', toggleSideNav);
+navContainer.addEventListener('click', function(e) {
+    if (e.target.closest('.nav-list__item')) {
+        toggleSideNav();
+        body.classList.toggle('scroll-disabled');
+    }
+});
 
 function toggleSideNav() {
     toggleButton.classList.toggle('open');
     navContainer.classList.toggle('open');
     body.classList.toggle('scroll-disabled');
 }
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+        delay: 3000
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination'
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+});
